9d5ca18fa0cbf53aaea1215ce584d32f24ac7efb
KL10Movies <65135315+muhssiin@users.noreply.github.com>
2021-07-30 00:16:03 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -125,7 +125,7 @@ async def auto_filter(bot, update):
                 ]
             )
         
-
+        return # return if no files found for that query
    else:
         await update.reply_text("No Results Foud For Your Request Please check the spelling of movie. Use Google to get correct spelling." 
                                 ,reply_markup=InlineKeyboardMarkup(
