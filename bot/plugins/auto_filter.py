e14faa740df7732d88b3b06427d454b30ef86eb0
KL10Movies <65135315+muhssiin@users.noreply.github.com>
2021-07-30 19:36:16 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -125,16 +125,7 @@ async def auto_filter(bot, update):
                 ]
             )
         
-        return
-
     else:
-        await update.reply_text("No Results Foud For Your Request Please check the spelling of movie. Use Google to get correct spelling." 
-                                ,reply_markup=InlineKeyboardMarkup(
-                                    [
-                                        [InlineKeyboardButton(text="𝚁𝚎𝚚𝚞𝚎𝚜𝚝", url="https://t.me/MCubeMediarequestbot")]
-                                                              ]
-                                                             )
-                                         )
         return # return if no files found for that query
     
 
